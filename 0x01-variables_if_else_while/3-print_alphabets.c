87f420777b7adac80f839c3283882fe72cb58893
justizzy17 <isrealukachukwuka@gmail.com>
2023-09-08 19:44:12 -0700

    done

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+ * main -  program that prints the alphabet in lowercase
+ * and then in uppercase.
+ * Return: 0 always
+ */
+
+int main (void)
+{
+	char loweCaseLetter ;
+	char upperCaseLetter ;
+
+	for (loweCaseLetter = 'a'; loweCaseLetter <= 'z'; loweCaseLetter++)
+		putchar (loweCaseLetter);
+	for (upperCaseLetter = 'A'; upperCaseLetter <= 'Z'; upperCaseLetter++)
+		putchar(upperCaseLetter);
+	putchar('\n');
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+ * main -  program that prints the alphabet in lowercase
+ * except letter q and e.
+ * Return: 0 always
+ */
+
+int main (void)
+{
+	char lowerCase;
+	for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++) {
+		if (lowerCase != 'q' && lowerCase != 'e')
+			putchar(lowerCase);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
