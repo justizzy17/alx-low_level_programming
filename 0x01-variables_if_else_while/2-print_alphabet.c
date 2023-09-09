c9fc2a50dc3ad4c1ccde5946a56ba81f476be5e1
justizzy17 <isrealukachukwuka@gmail.com>
2023-09-08 19:07:01 -0700

    print small letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * main -  program that prints the alphabet in lowercase
+ * Return: 0 always
+ */
+
+int main (void)
+{
+	char lowerCaseAlphabet;
+
+	for(lowerCaseAlphabet = 'a'; lowerCaseAlphabet <= 'z'; lowerCaseAlphabet++)
+	putchar (lowerCaseAlphabet);
+	putchar('\n');
+
+	return (0);
+}
+
