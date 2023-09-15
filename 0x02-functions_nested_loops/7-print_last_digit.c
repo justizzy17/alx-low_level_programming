1938f8c0582a7a6e4778aa3ad50248ac0fcd6201
justizzy17 <isrealukachukwuka@gmail.com>
2023-09-15 08:11:16 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,11 +10,12 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	if (n < 0)
-		n = -n;
-
 	last_digit = n % 10;
 
+	if (n < 0)
+	{
+		n = -n;
+	}
 	_putchar('0' + last_digit);
 	return (last_digit);
 }
