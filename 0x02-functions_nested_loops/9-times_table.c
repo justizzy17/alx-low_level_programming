7c268a99474c9ff54fb6a55089fb5a290ce40005
justizzy17 <isrealukachukwuka@gmail.com>
2023-09-15 09:55:06 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,8 +13,16 @@ void times_table(void)
 	{
 		for (b = 0; b < 10; b++)
 		{
+			if (calc < 10)
+			{
 			calc = a * b;
-			_putchar('0' + calc);
+			_putchar('0' + (calc % 10);
+			}
+			else
+			{
+			_putchar('0' + (calc / 10);
+			_putchar('0' + (calc % 10);
+			}
 
 			if (b != 9)
 			{
