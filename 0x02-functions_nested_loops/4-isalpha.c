99db6ba63da9e8ed701d1debef6ef88072eacd29
justizzy17 <isrealukachukwuka@gmail.com>
2023-09-15 06:54:16 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,15 +2,15 @@
 
 /**
  * _isalpha - checks for alphabetic character
- * @c:the character to be checked
- * Return: 1 if c is an alphabet, 0 otherwise
+ * @c: is a letter, lowercase or uppercase to be checked
+ * Return: 1 for alphabetic character or 0 for nothing else
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
 		return (1);
-	else if (c >= 'A' && c <= 'Z')
-		return (1);
+	}
 	return (0);
 }
