7c5c50de42734342faf3341ef857cef90f9231c6
justizzy17 <isrealukachukwuka@gmail.com>
2023-09-17 17:58:39 -0700

    done

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,13 +12,13 @@ void more_numbers(void)
 
 	for (a = 1; a <= 10; a++)
 	{
-		for (b = 0; b <= 15; b++)
+		for (b = 0; b < 15; b++)
 		{
-			if (b > 9)
-			{
-				_putchar('0' + (b / 10));
-			}
-				_putchar('0' + (b % 10));
+			if (b >= 10)
+		{
+			_putchar('0' + (b / 10));
+		}
+			_putchar('0' + (b % 10));
 		}
 		_putchar('\n');
 	}
