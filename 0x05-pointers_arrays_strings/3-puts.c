89c61cae6b71cd407c231db43d993f8deb00cd6f
justizzy17 <isrealukachukwuka@gmail.com>
2023-09-19 07:12:47 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,14 +8,11 @@
 
 void _puts(char *str)
 {
-	int a;
 
-	a = 0;
-
-	while (str[a] != '\0')
+	while (*str != '\0')
 	{
-		_putchar(str[a]);
-		a++;
+		_putchar(*str);
+		*str++;
 	}
 	_putchar('\n');
 }
