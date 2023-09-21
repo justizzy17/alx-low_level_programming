4f6bc0b8f1429de26bd5cb5ea53a73bc9a2abbd9
justizzy17 <isrealukachukwuka@gmail.com>
2023-09-21 09:35:35 -0700

    hard to understand

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * rev_string -  function that reverses a string
+ * @s: string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	char *first_letter = s;
+	char last_letter = s;
+	char swap;
+
+
+	while (*last_letter != '\0')
+	{
+		last_letter++;
+	}
+
+	if (last_letter > s)
+	{
+		last_letter--;
+	}
+
+	while (first_letter < last_letter)
+	{
+		swap = *first_letter;
+		*first_letter = *last_letter;
+		*last_letter = swap;
+
+		first_letter++;
+		last_letter--;
+	}
+}
