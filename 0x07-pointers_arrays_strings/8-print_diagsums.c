a122e91fff468a262f4da05dd6962f18c97b83e6
justizzy17 <isrealukachukwuka@gmail.com>
2023-09-30 19:02:10 -0700

    print diagonal sum

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - Entry point
+ * @a: input
+ * @size: input
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int sum1 = 0;
+	int sum2 = 0;
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 = sum1 + a[i * size + i];
+
+		sum2 = sum2 + a[(i * size) + (size - 1 - i)];
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
