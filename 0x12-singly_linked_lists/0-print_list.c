e47c2ef1ebbc453057f64e53b7144ff6bb81642f
justizzy17 <isrealukachukwuka@gmail.com>
2023-10-19 20:08:09 -0700

    0-print

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_list - function that prints all the elements of a list_t list
+ * @h: a struct pointer
+ * Return: size of struct
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
