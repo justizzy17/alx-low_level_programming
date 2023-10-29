9bd77cb2b5b9e42ed7305cdd74efc03b288d2476
justizzy17 <isrealukachukwuka@gmail.com>
2023-10-29 08:33:34 -0700

    corrected

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,17 +12,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	if (h == NULL)
-		return (0);
-	else
-	{
 		while (h != NULL)
 		{
 			printf("%d\n", h->n);
 		h = h->next;
 		count++;
 		}
-	}
 
 	return (count);
 }
