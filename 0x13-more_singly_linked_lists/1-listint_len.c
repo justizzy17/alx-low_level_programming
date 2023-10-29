9b6e2fc8a305d656f50320e1a7e437e91aec343b
justizzy17 <isrealukachukwuka@gmail.com>
2023-10-29 08:59:58 -0700

    done listing files

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * listint_len - function that returns the number of elements in a
+ * linked listint_t list.
+ * @h: struct data to be inputted
+ * Retrun: number of inputted struct data.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
