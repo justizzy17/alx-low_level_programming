ab452d84c313062af233a7673142282670dbd30b
justizzy17 <isrealukachukwuka@gmail.com>
2023-11-03 07:28:59 -0700

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,7 +13,7 @@ unsigned int binary_to_uint(const char *b)
 	int index;
 	unsigned int result = 0;
 
-	if (*b ==NULL)
+	if (*b == NULL)
 		return (0);
 
 	for (index = 0; b[index] != NULL; index++)
